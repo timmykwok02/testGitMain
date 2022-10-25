ae49e400307c6f4a81d2202e25ad771a38efc3c5
timmykwok02 <timmy.kwok1@email.kpu.ca>
2022-10-24 21:06:23 -0700

    test

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+    System.out.println("Hello world!");
+        int x = 5;
+        int y = 4;
+        int z = x + y;
+        System.out.println(x + "+" + y + "=" + z);
+
+    }
+}
