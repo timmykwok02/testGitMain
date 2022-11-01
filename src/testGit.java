ae13cf6a0a11e9593a35ff45609e39ebefcb25c1
timmykwok02 <timmy.kwok1@email.kpu.ca>
2022-10-31 18:22:24 -0700

    test

--- src/testGit.java
+++ src/testGit.java
@@ -1,3 +1,10 @@
 public class testGit {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        int x = 5;
+        int y = 4;
+        int z = x + y;
+        System.out.println(x + "+" + y + "=" + z);
+    }
 
 }
